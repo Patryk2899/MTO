3dd7696bd2fb94299371b91d5e8eb2d7fb25aba7
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2022-10-09 14:43:23 +0200

    Added display function which handles LowwerCase and UpperCase distinction

--- lab2/main.c
+++ lab2/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+void display(int size, char* param) {
+	for (int j=0;j<size;j++){
+		if (param[j] >= 65 && param[j] <= 90){
+			printf("%c", param[j] + 32);
+		}else if (param[j] >= 97 && param[j] <= 122 ){
+			printf("%c", param[j] - 32);
+		}else {
+			printf("%c", param[j]);
+		}
+	}
+}
+
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
