b6937fa82ebfff6c3fe878f9b11cf4d0612826c9
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2022-11-06 15:36:26 +0100

    Added int casting for input param

--- lab4/main.py
+++ lab4/main.py
@@ -2,18 +2,23 @@
 
 import sys
 
+def reverse_number(n):
+    r = 0
+    while n > 0:
+        r *= 10
+        r += n % 10
+        n /= 10
+    return r
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'g':
-                for i in range(0, len(param)):
-                    if param[i] != 0:
-                        break
-                    else:
-                        param = param[1:]
-                print(param[::-1],end="")
+                param = int(param)
+                param = reverse_number(param)
+                print(param,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
