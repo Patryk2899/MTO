35c8235ab9872aa674ac8719f63e6b718fb003a5
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2022-11-06 14:58:55 +0100

    Added test cases

--- lab4/main.py
+++ lab4/main.py
@@ -7,8 +7,8 @@ def my_printf(format_string,param):
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#' and format_string[idx+1] == 'g':
+                print(param[::-1],end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
