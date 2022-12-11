cc57176deaa3fc7e591a974c17a5566edac4933b
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2022-12-11 15:01:35 +0100

    Added missing comprasion

--- lab6/main.py
+++ lab6/main.py
@@ -23,7 +23,7 @@ def my_printf(format_string,param):
             if check == 4:
                 shouldSkip=False
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] and (format_string[idx+2] in digits) :
+            if format_string[idx] == '#' and format_string[idx+1] == '.' and (format_string[idx+2] in digits) :
                 if format_string[idx+3] == 'g':
                     x = int(format_string[idx+2])
                     n = len(param) - x
