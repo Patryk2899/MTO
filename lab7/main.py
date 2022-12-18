4a8f494b45ef18300d8615d9db956694b0044434
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2022-12-18 14:36:40 +0100

    Added function to change characters in hex number

--- lab7/main.py
+++ lab7/main.py
@@ -2,6 +2,25 @@
 
 import sys
 
+def change_characters(param):
+    hex_to_str = str(param)
+    count = 0
+    for i in hex_to_str:
+        if i == 'a':
+            hex_to_str[count] = 'g'
+        if i == 'b':
+            hex_to_str[count] = 'h'
+        if i == 'c':
+            hex_to_str[count] = 'i'
+        if i == 'd':
+            hex_to_str[count] = 'j'
+        if i == 'e':
+            hex_to_str[count] = 'k' 
+        if i == 'f':
+            hex_to_str[count] = 'l'
+        count = count + 1       
+    return hex_to_str
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
