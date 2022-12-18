e9dff0c5e5f83b009504b66b0607ff5634e502c1
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2022-12-18 14:52:39 +0100

    Fixed bug in change_characters function

--- lab7/main.py
+++ lab7/main.py
@@ -4,22 +4,24 @@ import sys
 
 def change_characters(param):
     hex_to_str = str(param)
-    count = 0
-    for i in hex_to_str:
-        if i == 'a':
-            hex_to_str[count] = 'g'
-        if i == 'b':
-            hex_to_str[count] = 'h'
-        if i == 'c':
-            hex_to_str[count] = 'i'
-        if i == 'd':
-            hex_to_str[count] = 'j'
-        if i == 'e':
-            hex_to_str[count] = 'k' 
-        if i == 'f':
-            hex_to_str[count] = 'l'
-        count = count + 1       
-    return hex_to_str
+    result = ""
+    for i in range(0, len(hex_to_str)):
+        if hex_to_str[i] == 'a':
+            temp = 'g'
+        elif hex_to_str[i] == 'b':
+            temp = 'h'
+        elif hex_to_str[i] == 'c':
+            temp = 'i'
+        elif hex_to_str[i] == 'd':
+            temp = 'j'
+        elif hex_to_str[i] == 'e':
+            temp = 'k' 
+        elif hex_to_str[i] == 'f':
+            temp = 'l'
+        else:
+            temp = hex_to_str[i]
+        result = result + temp
+    return result
 
 def my_printf(format_string,param):
     #print(format_string)
@@ -28,7 +30,7 @@ def my_printf(format_string,param):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'j':
                 hex_format = hex(int(param))
-                print(hex_format,end="")
+                print(change_characters(hex_format),end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
