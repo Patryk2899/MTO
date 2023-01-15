2b885a1f16a0f2163c6d87c7f4e2352b757751eb
Patryk Jozwiak <patryk.jozwiak@epassi.com>
2023-01-15 14:25:14 +0100

    Added size checking of an input

--- lab8/main.py
+++ lab8/main.py
@@ -2,8 +2,14 @@
 
 import sys
 
-def change_characters(param):
+def change_characters(param, count):
     hex_to_str = str(param)
+
+    if len(hex_to_str) < count:
+        count = count -len(hex_to_str)
+        for i in range(1, count):
+            "0" + hex_to_str
+
     result = ""
     for i in range(0, len(hex_to_str)):
         if hex_to_str[i] == 'a':
@@ -32,8 +38,9 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == '.' and format_string[idx+2] in numbers and format_string[idx+3] == "j" :
+                count = int(format_string[idx+2])
                 hex_format = hex(int(param))
-                print(change_characters(hex_format),end="")
+                print(change_characters(hex_format, count),end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
